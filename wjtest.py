e2aa67a1a250092ae7d4d108aa44bc949edc1922
Tyler Neylon <tylerneylon@gmail.com>
2011-11-03 09:40:43 -0700

    Cleanup in wjtest.py.

--- wjtest.py
+++ wjtest.py
@@ -21,6 +21,8 @@ def expectVsActual(expected, actual, comment=None):
 mark = wj._markFromUserTimeStr("345.2001")
 expectVsActual("345.2001", mark)
 
+# It would take a little more effort to make these
+# tests work at any time.
 if False:
   mark = wj._markFromUserTimeStr("today")
   expectVsActual("606.2011", mark)
@@ -40,69 +42,30 @@ if False:
   mark = wj._markFromUserTimeStr("last month")
   expectVsActual("5--.2011", mark)
 
-# TODO Clean this up into a loop.
-mark = wj._markFromUserTimeStr("Jan, 1988")
-expectVsActual("0--.1988", mark)
+testStrs = [["Jan, 1988", "0--.1988"],
+            ["December 1972", "10--.1972"],
+            ["12 1972", "10--.1972"],
+            ["06/01/79", "304.1979"],
+            ["0623423/01/793242", None],
+            ["9-11-01", "511.2001"],
+            ["Feb/29/2000", "113.2000"],
+            ["Feb/29/2001", None],
+            ["1 Jan 2025", "0.2025"],
+            ["1 February 1950", "43.1950"],
+            ["1 Feb,1950", "43.1950"],
+            ["30 Feb 3000", None],
+            ["10/22/11-10/28/11", "60-.2011"],
+            ["1/1-7/11", "0-.2011"],
+            ["Feb 5 - 11 2001", "5-.2001"],
+            ["9/3 - 9/9/1995", "50-.1995"],
+            ["1/29-2/4/88", "4-.1988"],
+            ["22 - 28 Oct 2011", "60-.2011"],
+            ["1-7 Jan 11", "0-.2011"],
+            ["29 Jan - 4 Feb 1988", "4-.1988"]]
 
-mark = wj._markFromUserTimeStr("December 1972")
-expectVsActual("10--.1972", mark)
-
-mark = wj._markFromUserTimeStr("12 1972")
-expectVsActual("10--.1972", mark)
-
-mark = wj._markFromUserTimeStr("06/01/79")
-expectVsActual("304.1979", mark)
-
-mark = wj._markFromUserTimeStr("0623423/01/793242")
-expectVsActual(None, mark)
-
-mark = wj._markFromUserTimeStr("9-11-01")
-expectVsActual("511.2001", mark)
-
-mark = wj._markFromUserTimeStr("Feb/29/2000")
-expectVsActual("113.2000", mark)
-
-mark = wj._markFromUserTimeStr("Feb/29/2001")
-expectVsActual(None, mark)
-
-mark = wj._markFromUserTimeStr("1 Jan 2025")
-expectVsActual("0.2025", mark)
-
-mark = wj._markFromUserTimeStr("1 February 1950")
-expectVsActual("43.1950", mark)
-
-mark = wj._markFromUserTimeStr("1 Feb,1950")
-expectVsActual("43.1950", mark)
-
-mark = wj._markFromUserTimeStr("30 Feb 3000")
-expectVsActual(None, mark)
-
-mark = wj._markFromUserTimeStr("10/22/11-10/28/11")
-expectVsActual("60-.2011", mark)
-
-mark = wj._markFromUserTimeStr("1/1-7/11")
-expectVsActual("0-.2011", mark)
-
-mark = wj._markFromUserTimeStr("Feb 5 - 11 2001")
-expectVsActual("5-.2001", mark)
-
-mark = wj._markFromUserTimeStr("9/3 - 9/9/1995")
-expectVsActual("50-.1995", mark)
-
-mark = wj._markFromUserTimeStr("1/29-2/4/88")
-expectVsActual("4-.1988", mark)
-
-userStr = "22 - 28 Oct 2011"
-mark = wj._markFromUserTimeStr(userStr)
-expectVsActual("60-.2011", mark, comment=userStr)
-
-userStr = "1-7 Jan 11"
-mark = wj._markFromUserTimeStr(userStr)
-expectVsActual("0-.2011", mark, comment=userStr)
-
-userStr = "29 Jan - 4 Feb 1988"
-mark = wj._markFromUserTimeStr(userStr)
-expectVsActual("4-.1988", mark, comment=userStr)
+for testCase in testStrs:
+  mark = wj._markFromUserTimeStr(testCase[0])
+  expectVsActual(testCase[1], mark, comment=testCase[0])
 
 print "fortune smiles upon you... for now - all tests passed"
-
+exit(0)
