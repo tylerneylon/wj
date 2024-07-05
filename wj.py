5e5c61fe83c1ae53a9a745b304d617c2f102bd8c
Amir Aref <amiraref808@gmail.com>
2024-07-05 19:21:06 +0330

    use new format of datetime.fromtimestamp method

--- wj.py
+++ wj.py
@@ -482,7 +482,7 @@ def _fromDayToScope(timeMark, scope="d", inputMode=None):
   elif scope == "w":
     if _userTimeMode == 'Greg':
       ts = _timestampForMark(timeMark)
-      d = datetime.datetime.utcfromtimestamp(ts)
+      d = datetime.datetime.fromtimestamp(ts, datetime.UTC)
       d1 = d + datetime.timedelta(days=(-1 * d.weekday()))
       d2 = d1 + datetime.timedelta(days=6)
       if d1.year != d2.year and d.year == d1.year: d2 = d1.replace(day=31)
@@ -496,7 +496,7 @@ def _fromDayToScope(timeMark, scope="d", inputMode=None):
   elif scope == "m":
     if _userTimeMode == 'Greg':
       ts = _timestampForMark(timeMark)
-      d = datetime.datetime.utcfromtimestamp(ts)
+      d = datetime.datetime.fromtimestamp(ts, datetime.UTC)
       tm = d.timetuple()
       d1 = d + datetime.timedelta(days=(1 - tm.tm_mday))
       daysInMonth = calendar.monthrange(tm.tm_year, tm.tm_mon)[1]
